3d0a76ba118aaecbf2e754b6b0acbf44b8a78c97
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 03:54:22 +0530

    cheque function is added

--- deposit.cpp
+++ deposit.cpp
@@ -1,16 +1,33 @@
 #include<iostream>
 using namespace std;
 
+void cheque()
+{
+    cout << "\n--------Cheque--------\n";
+    cout<<"\nInserting cheque into machine\n";
+    cout<<"\nEnter the amount of cheque: \n";
+    double amt;
+    cin >> amt;
+    cout<<"\nEnter sender's account number: \n";
+    int acc;
+    cin >> acc;
+    cout << "\nCheque Amount: " << amt << endl;
+    cout << "\nSender's Account Number: " << acc << endl;
+    cout<<"\nYour cheque is ready and it will be refelected in 7 days.\n";
+
+}
+
 void credentials()
 {
+    cout<<"\n--------Credentials--------\n";
     cout<<"\nInsert bank card to machine\n";
     cout <<"\nEnter your account number: \n";
     string acc;
     cin >> acc;
-    cout << "\nAccount Number: " << acc << endl;
     cout<<"\nEnter Receiver's account number: \n";
     string rec;
     cin >> rec; 
+    cout << "\nAccount Number: " << acc << endl;
     cout << "\nReceiver's Account Number: " << rec<< endl;
 }
 
@@ -18,6 +35,6 @@ int main()
 {
     cout<<"\n--------Deposit Cheque--------\n";
     credentials();
-    // cheque();
+    cheque();
     // transaction();
 }
